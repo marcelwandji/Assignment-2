9dd5fa5b9404573057799b8f08b7ba5dbcba8f04
marcelwandji <marcelwandjijr@gmail.com>
2019-12-04 23:12:22 +0100

    close #6

---
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// MARCEL JUNIOR WANDJI 1171044
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos.business.exception;
+
+public class TakeAwayBillException extends Exception{
+
+       private static final long serialVersionUID = 1L;
+       public TakeAwayBillException(final String str){
+           super(str);
+       }
+
+}
